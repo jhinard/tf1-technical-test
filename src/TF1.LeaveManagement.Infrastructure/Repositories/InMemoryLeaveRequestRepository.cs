5f52e01c910980c7dfd95438b6b4fa38888cc21c
jhinard <jeremyhinard@gmail.com>
2025-04-17 14:09:45 +0200

    🗃️ implement an in memory repository

---
+++ src/TF1.LeaveManagement.Infrastructure/Repositories/InMemoryLeaveRequestRepository.cs
@@ -0,0 +1,22 @@
+﻿using TF1.LeaveManagement.Application.LeaveRequests.Interfaces;
+using TF1.LeaveManagement.Domain.LeaveRequests;
+
+namespace TF1.LeaveManagement.Infrastructure.Repositories
+{
+    public class InMemoryLeaveRequestRepository : ILeaveRequestRepository
+    {
+        private readonly Dictionary<Guid, LeaveRequest> _inMemoryDatabase = new();
+
+        public Task<LeaveRequest?> GetByIdAsync(Guid id)
+        {
+            _inMemoryDatabase.TryGetValue(id, out var request);
+            return Task.FromResult(request);
+        }
+
+        public Task SaveAsync(LeaveRequest leaveRequest)
+        {
+            _inMemoryDatabase[leaveRequest.Id] = leaveRequest;
+            return Task.CompletedTask;
+        }
+    }
+}
